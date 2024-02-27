cf1844dfac33fac9e3868a8b73c72d51edaf17c2
najdorf <70437537+cn7249@users.noreply.github.com>
2024-02-27 12:10:25 +0900

    Feat : MakeBeatGrid 메서드 완성

--- Assets/Scripts/Sequencer/GridBtnsPreset.cs
+++ Assets/Scripts/Sequencer/GridBtnsPreset.cs
@@ -5,30 +5,45 @@ using UnityEngine;
 public class GridBtnsPreset : MonoBehaviour
 {
     [SerializeField] private GameObject _beat4;
+    [SerializeField] private GameObject _beat8;
+    [SerializeField] private GameObject _beat12;
+    [SerializeField] private GameObject _beat16;
+
     private GameObject _gridBtn;
+    private GameObject _line;
 
     private void Awake()
     {
         _gridBtn = Resources.Load<GameObject>("Prefabs/Sequencer/GridBtn");
+        _line = Resources.Load<GameObject>("Prefabs/Sequencer/Line");
     }
 
     private void Start()
     {
-        MakeBeatGrid();
+        MakeBeatGrid(4f, _beat4.transform);
+        MakeBeatGrid(8f, _beat8.transform);
     }
 
-    private void MakeBeatGrid()
+    private void MakeBeatGrid(float beat, Transform parent)
     {
+        float height = 640f / beat;
 
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < beat; i++)
         {
             for (int j = 0; j < 4; j++)
             {
-                float x = -240f + (160f * i);
-                float y = -320f + (160f * j);
+                float x = -240f + (160f * j);
+                float y = -320f + (height * i);
                 Vector3 pos = new Vector3(x, y, 0f);
 
-                Instantiate(_gridBtn, pos, Quaternion.identity, _beat4.transform);
+                _gridBtn.transform.localScale = new Vector3(1f, 4f/ beat, 1f);
+
+                Instantiate(_gridBtn, pos, Quaternion.identity, parent);
+            }
+
+            if (i > 0)
+            {
+                Instantiate(_line, new Vector3(0f, -320f + (height * i), 0f), Quaternion.identity, parent);
             }
         }
     }
