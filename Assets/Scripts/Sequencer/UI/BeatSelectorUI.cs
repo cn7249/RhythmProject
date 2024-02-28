5a514b32773903bfee3538d67481424a2afba661
najdorf <70437537+cn7249@users.noreply.github.com>
2024-02-29 02:03:50 +0900

    Feat : Beat Selector 구현 완료, (패턴 시퀀서 1차 완료)
    보완해야 할 기능(있으면 좋음)
    1. tempo, ms 입력 기능
    2. off grid 기능 넣기
    3. 패턴 미리보기(자동 스크롤)

--- Assets/Scripts/Sequencer/UI/BeatSelectorUI.cs
+++ Assets/Scripts/Sequencer/UI/BeatSelectorUI.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BeatSelectorUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Button _beat4Btn;
+    [SerializeField] private Button _beat8Btn;
+    [SerializeField] private Button _beat12Btn;
+    [SerializeField] private Button _beat16Btn;
+
+    private TextMeshProUGUI _beat4Txt;
+    private TextMeshProUGUI _beat8Txt;
+    private TextMeshProUGUI _beat12Txt;
+    private TextMeshProUGUI _beat16Txt;
+
+    private void Awake()
     {
-        
+        _beat4Btn.onClick.AddListener(() => SelectBeat4());
+        _beat8Btn.onClick.AddListener(() => SelectBeat8());
+        _beat12Btn.onClick.AddListener(() => SelectBeat12());
+        _beat16Btn.onClick.AddListener(() => SelectBeat16());
+
+        _beat4Txt = _beat4Btn.GetComponentInChildren<TextMeshProUGUI>();
+        _beat8Txt = _beat8Btn.GetComponentInChildren<TextMeshProUGUI>();
+        _beat12Txt = _beat12Btn.GetComponentInChildren<TextMeshProUGUI>();
+        _beat16Txt = _beat16Btn.GetComponentInChildren<TextMeshProUGUI>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SelectBeat4()
     {
-        
+        PatternManager.Instance.BeatType = BeatType.beat4;
+        SetAllTextDefault();
+        _beat4Txt.color = Color.green;
+    }
+
+    private void SelectBeat8()
+    {
+        PatternManager.Instance.BeatType = BeatType.beat8;
+        SetAllTextDefault();
+        _beat8Txt.color = Color.green;
+    }
+
+    private void SelectBeat12()
+    {
+        PatternManager.Instance.BeatType = BeatType.beat12;
+        SetAllTextDefault();
+        _beat12Txt.color = Color.green;
+    }
+
+    private void SelectBeat16()
+    {
+        PatternManager.Instance.BeatType = BeatType.beat16;
+        SetAllTextDefault();
+        _beat16Txt.color = Color.green;
+    }
+
+    private void SetAllTextDefault()
+    {
+        _beat4Txt.color = Color.white;
+        _beat8Txt.color = Color.white;
+        _beat12Txt.color = Color.white;
+        _beat16Txt.color = Color.white;
     }
 }
