71c8e88c34f0805424a5b75e700a9e031e1a3139
najdorf <70437537+cn7249@users.noreply.github.com>
2024-02-29 21:01:20 +0900

    Feat : 채보 불러와서 노트 내려오는 것까지 구현

---
+++ Assets/Scripts/InGame/NoteGenerator.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class NoteGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject _notes;
+
+    private GameObject _noteBlue;
+    private GameObject _noteOrange;
+
+    private XMLManager _xml;
+
+    private int tpm; // Default = 1536 ticks per 1 bar
+    private float hpt; // heights per tick = 640 / 1536
+    private float tempo;
+    private float hps;
+
+    private void Awake()
+    {
+        _xml = GetComponent<XMLManager>();
+        _noteBlue = Resources.Load<GameObject>("Prefabs/Notes/NoteBlue");
+        _noteOrange = Resources.Load<GameObject>("Prefabs/Notes/NoteOrange");
+    }
+
+    private void Start()
+    {
+        LoadNotes("xmlTest2.xml");
+    }
+
+    private void Update()
+    {
+        MoveNotes();
+    }
+
+    private void MoveNotes()
+    {
+        float speed = hps * Time.deltaTime;
+        _notes.transform.position -= new Vector3(0f, speed, 0f);
+    }
+
+    public bool LoadNotes(string fileName)
+    {
+        _xml.fileName = fileName;
+
+        if (File.Exists(_xml.GetFilePath))
+        {
+            _xml.LoadXML();
+            InitParameters();
+            MakeNotes();
+            return true;
+        }
+        else
+        {
+            return false; // 파일이 지정된 경로에 없으면 false 반환
+        }
+    }
+
+    private void InitParameters()
+    {
+        tpm = _xml.songInfos.tpm;
+        hpt = 640f / tpm;
+        tempo = _xml.songInfos.tempo;
+        hps = 8f / 3f * tempo;
+    }
+
+    private void MakeNotes()
+    {
+        // _xml.tracks의 정보로 Note를 생성
+        for (int i = 0; i < 4; i++)
+        {
+            foreach (int tick in _xml.tracks[i])
+            {
+                float x = -240f + (160f * i);
+                float y = 760f + tick * hpt; // 판정선 기준 bar 2개만큼 위에서 생성
+
+                if (i == 0 || i == 3) // 1번, 4번 트랙은 파란색 노트 생성
+                {
+                    Instantiate(_noteBlue, new Vector3(x, y, 0f), Quaternion.identity, _notes.transform);
+                }
+                else // 2번, 3번 트랙은 주황색 노트 생성
+                {
+                    Instantiate(_noteOrange, new Vector3(x, y, 0f), Quaternion.identity, _notes.transform);
+                }
+            }
+        }
+    }
+}
--- Assets/TestFolder/Scripts/Judge.cs
+++ Assets/TestFolder/Scripts/Judge.cs
@@ -12,29 +12,29 @@ enum JudgeType
 public class Judge : MonoBehaviour
 {
 	// 플레이어가 노트 키 입력 시 호출
-    public void JudgeNote()
-	{
-		// 최소 판정 범위 안에 노트가 없으면 리턴
-		if (true)
-			return;
-		// good 판정 범위 안에 노트가 있으면
-		if (true)
-		{
-			// perfect 판정 범위 안에 노트가 있으면 perfect 판정
-			if (true)
-			{
-				// to do perfect
-			}
-			// good 판정
-			else
-			{
-				// to do good
-			}
-		}
-		// bad 판정
-		else
-		{
-			// to do bad
-		}
-	}
+ //   public void JudgeNote()
+	//{
+	//	// 최소 판정 범위 안에 노트가 없으면 리턴
+	//	if (true)
+	//		return;
+	//	// good 판정 범위 안에 노트가 있으면
+	//	if (true)
+	//	{
+	//		// perfect 판정 범위 안에 노트가 있으면 perfect 판정
+	//		if (true)
+	//		{
+	//			// to do perfect
+	//		}
+	//		// good 판정
+	//		else
+	//		{
+	//			// to do good
+	//		}
+	//	}
+	//	// bad 판정
+	//	else
+	//	{
+	//		// to do bad
+	//	}
+	//}
 }
