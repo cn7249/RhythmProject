e708b07e0d457a78ccb4ec7d515a8dbabc2da3b9
najdorf <70437537+cn7249@users.noreply.github.com>
2024-03-05 02:44:53 +0900

    Feat : HP Bar UI 추가

--- Assets/TestFolder/Scripts/PlayerInfo.cs
+++ Assets/TestFolder/Scripts/PlayerInfo.cs
@@ -8,6 +8,7 @@ public class PlayerInfo : MonoBehaviour
     [SerializeField] private TMP_Text playerScore;
     [SerializeField] private TMP_Text gameSpeed;
     [SerializeField] private TMP_Text gameScore;
+    [SerializeField] private Transform hpBar;
 
     private void Start()
     {
@@ -18,6 +19,11 @@ public class PlayerInfo : MonoBehaviour
 
         UpdateScore();
         UpdateGameSpeed(InGameManager.instance.gameSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateGameHp(InGameManager.instance.hp);
     }
 
     private void UpdateScore()
@@ -32,5 +38,11 @@ public class PlayerInfo : MonoBehaviour
     private void UpdateGameSpeed(float speed)
     {
         gameSpeed.text = $"{speed.ToString("F1")}";
+    }
+
+    private void UpdateGameHp(int hp)
+    {
+        float barScale = (float) hp / InGameManager.instance.maxHp;
+        hpBar.localScale = new Vector3(1f, barScale, 1f);
     }
 }
